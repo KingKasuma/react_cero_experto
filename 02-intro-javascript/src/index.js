bf97d99e2038d43b0119a105fbdc37f11f32d8b7
Mike <lanselot777hailander@gmail.com>
2020-07-19 10:17:14 -0400

    Some changes

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,17 +1,1 @@
-// Variagles y Constantes
-
-const nombre = 'Fernando';
-const apellido = 'Herrera';
-
-let valorDado = 5;
-valorDado = 4;
-
-console.log(nombre, apellido, valorDado);
-
-// var No se debe de usar...
-if (true) {
-    const nombre = 'Peter';
-    console.log(nombre);
-}
-
-console.log( valorDado );
+console.log("hola mundo");
