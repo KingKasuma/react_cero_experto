8306cffa67bc24759063707df9eb0c7921c7d1c1
Mike <lanselot777hailander@gmail.com>
2023-12-07 17:27:18 -0400

    First component

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,8 @@
+export const FirstApp = () => {
+  return (
+    <>
+      <h1>Mike</h1>
+      <p>Soy un subtitulo</p>
+    </>
+  )
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
+import { FirstApp } from './FirstApp';
 
-import { HelloWorldApp } from './HelloWorldApp'
+
+// import { HelloWorldApp } from './HelloWorldApp'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorldApp />
+    <FirstApp />
   </React.StrictMode>
 )
